80f8e2056c062c6d3455b48acf51f1fa111082a9
yangchch6 <yangchch6@yonyou.com>
2019-07-13 16:37:18 +0800

    ac-hand-table

---
+++ tinper-acs/ac-hand-table/src/RefMultipleTable.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/prop-types,operator-linebreak,react/jsx-filename-extension */
+/**
+ *
+ * @title 基础示例1
+ * @description 表格参照，使用RefMultipleTableBaseUI,不带有input
+ *
+ */
+import React, { Component } from 'react';
+import RefMultipleTableBaseUI from 'ref-multiple-table';
+
+import 'ref-multiple-table/lib/index.css';
+
+class RefMultipleTable extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
+  render() {
+    const { showModal, currentRefType } = this.props;
+    return (currentRefType === 'refMultipleTable' && showModal ?
+        <RefMultipleTableBaseUI
+          {...this.props}
+        /> : null
+    );
+  }
+}
+
+export default RefMultipleTable;
